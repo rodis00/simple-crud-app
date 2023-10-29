fd06e9dd43d8611a1bf722cc6d234f5106844ac2
rodis00 <happyekkov@gmail.com>
2023-10-29 10:54:18 +0100

    add network error message

--- frontend/src/component/BookList.js
+++ frontend/src/component/BookList.js
@@ -4,6 +4,7 @@ import BookService from "../service/BookService";
 
 const BookList = () => {
   const [books, setBooks] = useState([]);
+  const [networkError, setNetworkError] = useState("");
 
   useEffect(() => {
     getAllBooks();
@@ -15,7 +16,10 @@ const BookList = () => {
         setBooks(res.data);
         console.log(res);
       })
-      .catch((e) => console.log(e));
+      .catch((e) => {
+        setNetworkError(e.code);
+        console.log(networkError);
+      });
   }
 
   function deleteBookById(id) {
@@ -32,41 +36,49 @@ const BookList = () => {
       <Link to={"/add-book"} className="btn btn-primary mb-2 mt-3" href="#">
         Add Book
       </Link>
-      {books.length > 0 ? (
+      {networkError === "" ? (
         <>
-          <h2 className="text-center mb-4">Book List</h2>
-          <table className="table table-bordered table striped">
-            <thead>
-              <th>Book ID</th>
-              <th>Book Title</th>
-              <th>Book Raiting</th>
-              <th>Actions</th>
-            </thead>
-            <tbody>
-              {books.map((book) => (
-                <tr id={book.id}>
-                  <td>{book.id}</td>
-                  <td>{book.title}</td>
-                  <td>{book.rating}</td>
-                  <td>
-                    <Link to={`/books/${book.id}`} className="btn btn-info">
-                      Update
-                    </Link>
-                    <a
-                      className="btn btn-danger"
-                      onClick={() => deleteBookById(book.id)}
-                    >
-                      Delete
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          {books.length > 0 ? (
+            <>
+              <h2 className="text-center mb-4">Book List</h2>
+              <table className="table table-bordered table striped">
+                <thead>
+                  <th>Book ID</th>
+                  <th>Book Title</th>
+                  <th>Book Raiting</th>
+                  <th>Actions</th>
+                </thead>
+                <tbody>
+                  {books.map((book) => (
+                    <tr id={book.id}>
+                      <td>{book.id}</td>
+                      <td>{book.title}</td>
+                      <td>{book.rating}</td>
+                      <td>
+                        <Link to={`/books/${book.id}`} className="btn btn-info">
+                          Update
+                        </Link>
+                        <a
+                          className="btn btn-danger"
+                          onClick={() => deleteBookById(book.id)}
+                        >
+                          Delete
+                        </a>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </>
+          ) : (
+            <div className="container text-center">
+              <h3>No data to display.</h3>
+            </div>
+          )}
         </>
       ) : (
         <div className="container text-center">
-          <h3>No data to display.</h3>
+          <h3>{networkError}</h3>
         </div>
       )}
     </div>
